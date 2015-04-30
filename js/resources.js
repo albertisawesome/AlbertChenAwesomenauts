ad32e751626f40b5be7905e2d968868683fb0ea8
Albert Chen <chenalbert60@gmail.com>
2015-04-30 14:13:46 -0700

    spear doesnt work

--- js/resources.js
+++ js/resources.js
@@ -4,15 +4,36 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+        
+        //files for all the images.
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-        {name: "player", type:"image", src: "data/img/orcSpear.png"},
-        {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
-        {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
-        {name: "title-screen", type:"image", src: "data/img/title.png"},
-        {name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
-        {name: "gold-screen", type:"image", src: "data/img/spend.png"},
-        //{name: "minimap", type:"image", src: "data/img/minimap3.png"},
+        //{name: "custom-background-tile", type:"image", src: "data/img/custom-background-tile.png"},
+        
+        {name: "creep1", type: "image", src: "data/img/brainmonster.png"},
+        {name: "creep2", type: "image", src: "data/img/gloop.png"},
+        
+        {name: "player", type: "image", src: "data/img/orcSpear.png"},
+        {name: "player2", type: "image", src: "data/img/archer.png"},
+        {name: "player3", type: "image", src: "data/img/wizard.png"},
+        {name: "player4", type: "image", src: "data/img/skeletonDagger.png"},
+        {name: "player5", type: "image", src: "data/img/elf.png"},
+        
+        {name: "spear", type: "image", src: "data/img/spear.png"},
+        {name: "arrow", type: "image", src: "data/img/arrow.png"},
+        {name: "fireball", type: "image", src: "data/img/fireball.png"},
+        
+        {name: "tower", type: "image", src: "data/img/tower_round.svg.png"},
+        //{name: "minimap", type: "image", src: "data/img/MiniMapImage2.png"},
+        
+        {name: "title-screen", type: "image", src: "data/img/title.png"},
+        {name: "exp-screen", type: "image", src: "data/img/loadpic.png"},
+        {name: "gold-screen", type: "image", src: "data/img/spend.png"},
+        {name: "load-screen", type: "image", src: "data/img/loadpic.png"},
+        {name: "new-screen", type: "image", src: "data/img/newpic.png"},
+        {name: "char_select", type: "image", src: "data/img/char_select.png"},
+        {name: "pause", type: "image", src: "data/img/pause.png"},
+
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -23,13 +44,15 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+        
         {name: "level01", type: "tmx", src: "data/map/test.tmx"},
-
 
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */	
+//        {name: "gamebackground", type: "audio", src: "https://www.youtube.com/watch?v=TvjPa4RnSxY"},
+//        {name: "example_bgm", type: "audio", src: "data/bgm/Gorment_Race.xspf"}
 
 	/* Sound effects. 
 	 * @example
